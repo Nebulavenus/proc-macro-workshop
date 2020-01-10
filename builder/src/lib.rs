a8d156fcb10507f800d5f4723da1171d691c9ae5
Nebula Venus <6595784+Nebulavenus@users.noreply.github.com>
2020-01-10 17:57:04 +0600

    02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,11 +2,35 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    println!("{:#?}", ast);
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = syn::Ident::new(bname.as_ref(), name.span());
+    //println!("{:#?}", ast);
 
-    TokenStream::new()
+    let expanded = quote! {
+        pub struct #bident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #name {
+            pub fn builder() -> #bident {
+                #bident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    expanded.into()
 }
